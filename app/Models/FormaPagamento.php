2995fe2041c197f4d12d7ff9453364c1c5f3ba34
Felipe Bentes dos Santos <felipbents@gmail.com>
2025-10-17 17:53:13 -0300

    feat: Conclui a estrutura do banco de dados com todas as migrations

---
+++ app/Models/FormaPagamento.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormaPagamento extends Model
+{
+
+    protected $table = 'formas_pagamento';
+
+    use HasFactory;
+}
